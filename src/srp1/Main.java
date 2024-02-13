10aea7eef5147700813503220cb0c014ba45b7b1
Aleksei Kovalev <akovalevgm@mail.ru>
2024-02-13 22:08:30 +0400

    add seminar 6

--- src/srp1/Main.java
+++ src/srp1/Main.java
@@ -2,11 +2,19 @@ package srp1;
 
 import srp1.figure.Point;
 import srp1.figure.Square;
+import srp1.figure.ShapeDrawer;
 
 public class Main {
+
+    static ShapeDrawer squareDrawer = new ShapeDrawer() {
+        @Override
+        public void draw(Square square) {
+
+        }
+    };
     public static void main(String[] args) {
         Square square = new Square(new Point(1,1), 5);
         System.out.printf("Square area: %d \n", square.getArea());
-        square.draw();
+        squareDrawer.draw(square);
     }
 }
