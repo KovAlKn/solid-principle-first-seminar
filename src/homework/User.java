28b97c40827d83770c1a9e3bd571d059a1a9160e
Aleksei Kovalev <akovalevgm@mail.ru>
2024-02-14 22:11:42 +0400

    HomeWork6

--- src/homework/User.java
+++ src/homework/User.java
@@ -1,6 +1,6 @@
 package homework;
 
-public class User{
+public class User implements Saveable,Reportable{
     private final String name;
 
     public User(String name){
@@ -11,12 +11,14 @@ public class User{
         return name;
     }
 
-    public void save(){
-        Persister persister = new Persister(this);
-        persister.save();
+    @Override
+    public void report() {
+        System.out.println("Report user: " + name);
     }
 
-    public void report(){
-        System.out.println("Report for user: " + name);
+    @Override
+    public void save() {
+        System.out.println("Save user: " + name);
+
     }
 }
